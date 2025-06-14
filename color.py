1cb16c892f162607959a83a6703f53a70c22a4a4
Beniter-Tumusiime <beniterblue@gmail.com>
2025-06-14 17:51:07 +0300

    Add files via upload

---
+++ color.py
@@ -0,0 +1,10 @@
+print("Enter favorite color")
+color = input("color: ")  
+if color == "red":
+    print("You like red")
+elif color == "blue":
+    print("You like blue")
+elif color == "green":
+    print("You like green")
+else:
+    print("You like other colors")
